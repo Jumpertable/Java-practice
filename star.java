5d3c071613375265a754be089a6520f7bfa34bb4
Charlotte <6730613009@psu.ac.th>
2025-06-11 17:11:24 +0100

    Starter

---
+++ star.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class star {
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+int i,j,n;
+System.out.print("Enter n: ");
+n = scan.nextInt();
+for(i=1; i<=n; i++){
+for(j=1; j<=n; j++){
+if(i==j)
+System.out.print("*");
+else
+System.out.print(" ");
+
+}
+System.out.println();
+}
+scan.close();
+}
+}
