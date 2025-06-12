655b018a65f307de61f321127ce6b7d9062c16f3
Charlotte <6730613009@psu.ac.th>
2025-06-12 06:23:25 +0100

    box

--- star.java
+++ star.java
@@ -7,10 +7,10 @@ System.out.print("Enter n: ");
 n = scan.nextInt();
 for(i=1; i<=n; i++){
 for(j=1; j<=n; j++){
-if(i==j)
+if(i==1 || i==n || j==1 || j==n)
 System.out.print("*");
 else
-System.out.print("-");
+System.out.print(" ");
 
 }
 System.out.println();
