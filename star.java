22c6b9907ac3e92694d459f292eda6aba082c292
Charlotte <6730613009@psu.ac.th>
2025-06-16 10:02:58 +0100

    ehhh

--- star.java
+++ star.java
@@ -2,16 +2,38 @@ import java.util.Scanner;
 class star {
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
-int i,j,n;
+int i,j,k,n,m;
 System.out.print("Enter n: ");
 n = scan.nextInt();
-for(i=1; i<=n; i++){
-for(j=1; j<=n; j++){
-if(i==1 || i==n || j==1 || j==n)
-System.out.print("*");
-else
-System.out.print(" ");
-
+for(i=1; i<=n; i+=2)
+{ for(j=1; j<=(n-i); j++)
+{ System.out.print(" ");
+}
+for(k=1; k<=i; k++)
+{ System.out.print("*");
+}
+for(m=1; m<=i; m++)
+{ System.out.print("*");
+}
+for(j=1; j<=2*(n-i); j++)
+{ System.out.print(" ");
+}
+for(k=1; k<=i; k++)
+{
+    System.out.print("*");
+}
+for(m=1; m<=i; m++)
+{ System.out.print("*");
+}
+System.out.println();
+}
+for(i=1; i<=n; i++)
+{ for(j=1; j<=2*(i-1); j++)
+{ System.out.print(" ");
+}
+for(k=1; k<=4*(n-i+1); k++)
+{
+    System.out.print("*");
 }
 System.out.println();
 }
