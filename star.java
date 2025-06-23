4a30644e0b1cd64f3c48ec12ca72cdfc7bd204cd
Charlotte <6730613009@psu.ac.th>
2025-06-23 19:44:20 +0100

    Progress 2

--- star.java
+++ star.java
@@ -2,54 +2,89 @@ import java.util.Scanner;
 class star {
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
-int i,j,k,n,m;
+int i,j,k,n,m,l;
 System.out.print("Enter n: ");
 n = scan.nextInt();
 
-for(i=1; i<=n; i+=2)
-{ for(j=1; j<=(n-i); j++)
-{ System.out.print("-");
-}
-for(k=1; k<=i; k++)
-{ System.out.print("*");
-}
-for(m=1; m<=i; m++)
-{ System.out.print("*");
-}
-for(j=2; j<=2*(n-i); j++)
-{ System.out.print("-");
-}
-for(k=1; k<=i; k++)
-{
-    System.out.print("*");
-}
-for(m=1; m<=i; m++)
-{ System.out.print("*");
-}
-System.out.println();
-}
 for(i=1; i<=n; i++)
 {
-    for(j=2; j<=i; j++)
-{ System.out.print(" ");
-}
-for(m=1; m<=2*(n-i+1); m++)
+    for (l = 0; l <= n; l++) {
+        if (i == 1 || i == n || l == 1 || l == n)
+            System.out.print("");
+    }
+    System.out.println();
+    for(i=1; i<=n; i++)
 {
-    System.out.print("*");
+    for (l = 0; l <= n; l++) {
+        if (i == 1 || i == n || l == 1 || l == n)
+            System.out.print( "  -- ");
+        else
+            System.out.print(" ");
+    }
 }
-for(j=1; j<=i; j++)
+    System.out.println();
+
+        
+    for(i=1; i<=n; i++)
 {
-    System.out.print(" ");
+    for (m = 0; m <= n; m++) {
+        if (i == 1 || i == n || m == 1 || m == n)
+            System.out.print( " *-- ");
+        else
+            System.out.print(" ");
+    }
 }
-for(j=1; j<=i; j++)
+    System.out.println();
+
+
+
+for(i=1; i<=n; i++)
 {
-    System.out.print(" ");
+    for (m = 0; m <= n; m++) {
+        if (i == 1 || i == n || m == 1 || m == n)
+            System.out.print( " ----");
+        else
+            System.out.print(" ");
+    }
 }
-for(k=1; k<=2*(n-i+1); k++)
+    System.out.println();
+
+for(i=1; i<=n; i++)
 {
-    System.out.print("*");
+    for (j = 0; j <= n; j++) {
+        if (i == 1 || i == n || j == 1 || j == n)
+            System.out.print("   0 ");
+        else
+            System.out.print(" ");
+    }
 }
 
+System.out.println();
+
+
+for(k=1; k<=n; k++)
+{
+    for (l = 0; l <= n; l++) {
+        if (k == 1 || k == n || l == 1 || l == n)
+            System.out.print("  -|-");
+        else
+            System.out.print(" ");
+
+    }
+}
+System.out.println();
+
+
+for(k=1; k<=n; k++)
+{
+    for (l = 0;l <= n; l++) {
+        if (k == 1 || k == n || l == 1 ||l == n)
+            System.out.print(" _/\\_");
+        else
+            System.out.print(" ");
+
+    }
+}
 
 
 System.out.println();
